6580137328778d56681aaa8a445c74c427d4fdd9
uyeexnnhi <phamnhi936@gmail.com>
2023-12-16 18:55:34 +0700

    payment_done

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -15,7 +15,12 @@ import { useNavigate } from "react-router-dom";
 
 
 
+
 const Recentpayment = ({ recentpayment }) => {
+    const navigate = useNavigate();
+    const handleEditButtonClick = (paymentId) => {
+        navigate(`/EditAppointmentForm/${paymentId}`);
+    };
 
     const itemsPerPage = 6;
 
@@ -38,7 +43,7 @@ const Recentpayment = ({ recentpayment }) => {
         <div>
 
 
-            <div className=' px-4 mt-4 border-none bg-gray-200 grid grid-cols-[2fr,2fr,2fr,2fr,2fr,2fr] rounded-mds  font-bold py-4'>
+            <div className=' px-4 mt-4 border-none bg-gray-200 grid grid-cols-[2fr,2fr,2fr,2fr,2fr,2fr,1fr] rounded-mds  font-bold py-4'>
 
                 <p>Number</p>
                 <p>Date</p>
@@ -46,12 +51,13 @@ const Recentpayment = ({ recentpayment }) => {
                 <p>Paid Money</p>
                 <p>Type Pay</p>
                 <p>Note</p>
+                <p> </p>
 
             </div>
             <div className="mt-4 flex flex-col gap-1 ">
                 {visiblepayments.map((payment, index) => (
 
-                    <div key={index} className='w-full grid grid-cols-[2fr,2fr,2fr,2fr,2fr,2fr] items-center hover:bg-black-400/50 py-2 p-4 rounded-2xl cursor-pointer mb-2 bg-white'>
+                    <div key={index} className='w-full grid grid-cols-[2fr,2fr,2fr,2fr,2fr,2fr,1fr] items-center hover:bg-black-400/50 py-2 p-4 rounded-2xl cursor-pointer mb-2 bg-white'>
                         <h3 className='font-bold text-base text-100 '>
                             {payment.PAYMENT_ID}
                         </h3>
@@ -85,6 +91,15 @@ const Recentpayment = ({ recentpayment }) => {
                         </div>
                         <p>{payment.TYPE_PAY}</p>
                         <p>{payment.PAYMENT_NOTE}</p>
+                        <Button
+                            id="editappointment"
+                            onClick={() => handleEditButtonClick(payment.PAYMENT_ID)}
+                            //onClick={handleSignIn}
+                            className="border-none  bg-purple-500 py-4 px-4 flex flex-row items-center gap-2">
+
+                            <FaPlus className="flex" size={15} />
+                            <p className="flex">Edit</p>
+                        </Button>
 
                     </div>
                 ))}
@@ -128,7 +143,7 @@ const Payment = () => {
         <div className="flex flex-col my-5">
             <div className="flex flex-row mb-4 items-center ">
 
-                <h2 className=" flex-grow text-black font-bold">payment List</h2>
+                <h2 className=" flex-grow text-black font-bold">Payment List</h2>
                 <Button
                     id="addpayment"
                     onClick={handleAddButtonClick}
